0ab9183facaabd03926591ffd2df640dbd4a142f
Sandeep Sahani <sylveryte@protonmail.com>
2025-09-24 08:21:20 +0530

    Updated sylmark grammar and minor bug fix with show command

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module sylmark
 go 1.23.6
 
 require (
-	codeberg.org/sylveryte/tree-sitter-sylmark v0.1.42
+	codeberg.org/sylveryte/tree-sitter-sylmark v0.1.43
 	github.com/sourcegraph/jsonrpc2 v0.2.0
 	github.com/tree-sitter/go-tree-sitter v0.25.0
 )
--- lspserver/window_show_document.go
+++ lspserver/window_show_document.go
@@ -6,26 +6,18 @@ import (
 	"sylmark/lsp"
 )
 
-// returns isSucess
 func (h *LangHandler) ShowDocument(uri lsp.DocumentURI, external bool, rng lsp.Range) error {
 
-	result := lsp.ShowDocumentResult{}
-	// ctx  := context.WithTimeout(context.Background(), time.Second*3)
-	ctx := context.Background()
-	err := h.Connection.Call(ctx, "window/showDocument",
+	err := h.Connection.Notify(context.Background(), "window/showDocument",
 		lsp.ShowDocumentParams{
 			URI:       uri,
 			External:  external,
 			Selection: rng,
 			TakeFocus: true,
 		},
-		&result,
 	)
 	if err != nil {
 		return fmt.Errorf("failed to call window/showDocument: %w", err)
-	}
-	if !result.Success {
-		return fmt.Errorf("client failed to open document")
 	}
 	return nil
 }
